2d5a327f043bdee1556daf2103ad34a6eedb42a1
Carl Thomé <carlthome@gmail.com>
2023-08-21 01:21:24 +0200

    One post per index page

--- conf.py
+++ conf.py
@@ -1183,7 +1183,7 @@ MARKDOWN_EXTENSIONS = [
 
 # Modify the number of Post per Index Page
 # Defaults to 10
-# INDEX_DISPLAY_POST_COUNT = 10
+INDEX_DISPLAY_POST_COUNT = 1
 
 # By default, Nikola generates RSS files for the website and for tags, and
 # links to it.  Set this to False to disable everything RSS-related.
